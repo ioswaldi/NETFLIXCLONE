e08435e8c00c089f5d2503422990bf963c980dac
waldifebrianda <waldifebriandaios@gmail.com>
2023-08-02 22:07:03 +0700

    Initial Commit

---
+++ NETFLIXCLONE/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NETFLIXCLONE
+//
+//  Created by Waldi Febrianda on 02/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
